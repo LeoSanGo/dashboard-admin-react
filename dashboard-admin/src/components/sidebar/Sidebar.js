afab7abd41285711e2244dbdea7d73f48e0e2fe7
leosango <souoleo@gmail.com>
2022-01-13 23:52:50 -0300

    initial code sidebar

--- dashboard-admin/src/components/sidebar/Sidebar.js
+++ dashboard-admin/src/components/sidebar/Sidebar.js
@@ -5,9 +5,29 @@ const Sidebar = ({ sidebarOpen, sidebarClose }) => {
   return (
     <div className={sidebarOpen ? "sidebar-responsive" : ""}>
       <div className='sidebar__tutle'>
-        <div className='sidebar__'>
-
+        <div className='sidebar__img'>
+          <img src={logo} alt="Logo" />
+          <h1>DASHBOARDS</h1>
         </div>
+
+        <i 
+        onClick={() => closeSidebar()}
+        className='fa fa-times'
+        id='sidebarIcon'
+        aria-hidden="true"
+        ></i>
+      </div>
+      <div className='sidebar__menu'>
+        <div className='sidebar__link acitive_menu_link'>
+          <i className='fa fa-minus-square'></i> 
+          <a href='#'>Home</a>
+        </div>
+        <h2>ADMIN</h2>
+        <div className='sidebar__link'>
+          <i className='fa fa-tachometer'></i> 
+          <a href='#'>Área administrativa</a>
+        </div>
+        
       </div>
     </div>
   );
